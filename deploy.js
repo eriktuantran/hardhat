2d61a9cde0ae58d969109d6433c96f0df0d842bf
Tuan Tran <tuan.tran@smartoptics.com>
2023-02-12 12:29:01 +0100

    url + private key in env

--- deploy.js
+++ deploy.js
@@ -1,13 +1,9 @@
 const ethers = require("ethers");
 const fs = require("fs-extra");
+require("dotenv").config();
 async function main() {
-  const provider = new ethers.providers.JsonRpcProvider(
-    "http://127.0.0.1:7545"
-  );
-  const wallet = new ethers.Wallet(
-    "30cffc37a9bafec2b2d285eb75c6dd8468b2f93ba8012c914e3b2b86e49a02c3",
-    provider
-  );
+  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
   const abi = await fs.readFileSync(
     "./SimpleStorage_sol_SimpleStorage.abi",
@@ -21,13 +17,14 @@ async function main() {
   console.log("Deploying, please wait...");
   const contract = await contractFactory.deploy();
   const transactionReceipt = await contract.deployTransaction.wait(1);
-  console.log("Here is the deployment transaction: ");
-  console.log(contract.deployTransaction);
-  console.log("Here is the tranaction receipt: ");
-  console.log(transactionReceipt);
-
   const currentFavoriteNumber = await contract.retrieve();
-  console.log(currentFavoriteNumber);
+  console.log(`Current favorite number is ${currentFavoriteNumber}`);
+
+  const trans = await contract.store(10);
+  const receipt = trans.wait(1);
+
+  const newNumber = await contract.retrieve();
+  console.log(`New number is ${newNumber}`);
 }
 
 main()
