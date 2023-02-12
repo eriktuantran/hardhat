a3e0d6eecd69cacc672b9a22d34f38d92e6a4545
Tuan Tran <tuan.tran@smartoptics.com>
2023-02-12 10:00:44 +0100

    current favorite number

--- deploy.js
+++ deploy.js
@@ -1,14 +1,33 @@
+const ethers = require("ethers");
+const fs = require("fs-extra");
 async function main() {
-  const [deployer] = await ethers.getSigners();
+  const provider = new ethers.providers.JsonRpcProvider(
+    "http://127.0.0.1:7545"
+  );
+  const wallet = new ethers.Wallet(
+    "30cffc37a9bafec2b2d285eb75c6dd8468b2f93ba8012c914e3b2b86e49a02c3",
+    provider
+  );
 
-  console.log("Deploying contracts with the account:", deployer.address);
+  const abi = await fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.abi",
+    "utf-8"
+  );
+  const binary = await fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf-8"
+  );
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying, please wait...");
+  const contract = await contractFactory.deploy();
+  const transactionReceipt = await contract.deployTransaction.wait(1);
+  console.log("Here is the deployment transaction: ");
+  console.log(contract.deployTransaction);
+  console.log("Here is the tranaction receipt: ");
+  console.log(transactionReceipt);
 
-  console.log("Account balance:", (await deployer.getBalance()).toString());
-
-  const Token = await ethers.getContractFactory("Token");
-  const token = await Token.deploy();
-
-  console.log("Token address:", token.address);
+  const currentFavoriteNumber = await contract.retrieve();
+  console.log(currentFavoriteNumber);
 }
 
 main()
